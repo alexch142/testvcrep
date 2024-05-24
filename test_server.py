8f62d7762559088f2c325c63859b2ce98b9adaba
alexch142 <37656860+alexch142@users.noreply.github.com>
2024-05-24 12:21:41 +0300

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -8,22 +8,22 @@ def client():
     tested_app.app.config['TESTING'] = True
     with tested_app.app.test_client() as client:
         yield client
-        
+
 
 def test_get_hello_endpoint(client):
     r = client.get('/')
     assert r.data == b'Hello World!'
-    
+
 
 def test_post_hello_endpoint(client):
     r = client.post('/')
     assert r.status_code == 405
-    
+
 
 def test_get_api_endpoint(client):
     r = client.get('/api')
     assert r.json == {'status': 'test'}
-    
+
 
 def test_correct_post_api_endpoint(client):
     r = client.post('/api',
@@ -37,7 +37,7 @@ def test_correct_post_api_endpoint(client):
                     data=json.dumps({'name': 'Den'}))
     assert r.json == {'status': 'OK'}
     assert r.status_code == 200
-    
+
 
 def test_not_dict_post_api_endpoint(client):
     r = client.post('/api',
@@ -45,7 +45,7 @@ def test_not_dict_post_api_endpoint(client):
                     data=json.dumps([{'name': 'Den'}]))
     assert r.json == {'status': 'bad input'}
     assert r.status_code == 400
-    
+
 
 def test_no_name_post_api_endpoint(client):
     r = client.post('/api',
@@ -53,7 +53,7 @@ def test_no_name_post_api_endpoint(client):
                     data=json.dumps({'age': 100}))
     assert r.json == {'status': 'bad input'}
     assert r.status_code == 400
-    
+
 
 def test_bad_age_post_api_endpoint(client):
     r = client.post('/api',
